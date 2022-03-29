6df6f4f11cfcf5d927d41fd6ff654a65209d92a8
mmattbtw <matt@mmatt.net>
2022-03-29 18:16:25 -0500

    init

---
+++ src/go.mod
@@ -0,0 +1,7 @@
+module main
+
+go 1.18
+
+require github.com/gempir/go-twitch-irc/v3 v3.0.0
+
+require github.com/joho/godotenv v1.4.0
---
+++ src/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gempir/go-twitch-irc/v3"
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func main() {
+	client := twitch.NewClient(os.Getenv("TWITCH_USER"), os.Getenv("TWITCH_AUTH"))
+
+	client.OnPrivateMessage(func(message twitch.PrivateMessage) {
+		if message.Tags["first-msg"] != "0" {
+			client.Say(message.Channel, "@"+message.User.DisplayName+" have you seen this yet? https://i.nuuls.com/XHHZk.jpeg")
+		}
+	})
+
+	client.Join(os.Getenv("TWITCH_CHANNEL"))
+	// client.Say("mmattbtw", "TriHard hello from go")
+
+	err := client.Connect()
+	if err != nil {
+		panic(err)
+	}
+}
